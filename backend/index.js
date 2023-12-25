ddf53f15d805d22ea71c01a9abc75aac0d78cfe2
AbhishekNryn <narayanabhishek090@gmail.com>
2023-12-26 02:01:59 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const { default: axios } = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+
+  try {
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      { "username": "username", "secret": "username","firstname":"username"},
+      { headers: { "private-key": "348ef0d1-402a-476c-9590-6ecbadb4dd51" } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+
+app.listen(3001);
