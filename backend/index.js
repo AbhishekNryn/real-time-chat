7f6f1db70e1be02f630eef53961e8259879a793b
AbhishekNryn <narayanabhishek090@gmail.com>
2023-12-28 13:51:43 +0530

    ui done

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { default: axios } = require("axios");
+require('dotenv').config();
 
 const app = express();
 app.use(express.json());
@@ -12,13 +13,13 @@ app.post("/authenticate", async (req, res) => {
   try {
     const r = await axios.put(
       "https://api.chatengine.io/users/",
-      { "username": "username", "secret": "username","firstname":"username"},
-      { headers: { "private-key": "348ef0d1-402a-476c-9590-6ecbadb4dd51" } }
+      { username: username, secret: username, first_name: username },
+      { headers: { "private-key":process.env.PRIVATE_KEY } }
     );
     return res.status(r.status).json(r.data);
   } catch (e) {
-    return res.status(e.response.status).json(e.response.data);
+    console.error("Error:", e); // Log the detailed error for debugging
+    return res.status(500).json({ error: "Internal Server Error" });
   }
 });
-
 app.listen(3001);
